7519805fc9f2004dfe6f264faac9a2a84251e220
benjamin luton <benjamin@luton.net.au>
2022-01-30 16:25:51 +1000

    bedrock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Bedrock'
+copyright = '2022, Papina'
+author = 'Papina'
 
-release = '0.1'
-version = '0.1.0'
+release = '3.0'
+version = '3.0.0'
 
 # -- General configuration
 
